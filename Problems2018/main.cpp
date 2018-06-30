81a208a1b05cf73c984bb3db138c95c9199f7269
Roye Li <royetest02@hotmail.com>
2018-06-30 11:20:45 -0700

    Initial Commit

---
+++ Problems2018/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Problems2018
+//
+//  Created by Roye Li on 6/30/18.
+//  Copyright © 2018 Roye Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
